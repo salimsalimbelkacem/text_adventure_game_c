b025041b7cbd0c27e42d04dda898825d03d7a2ab
salim <salim.belkacemm@protonmail.com>
2025-10-18 21:28:01 +0100

    game loop and simple io system

--- src/main.c
+++ src/main.c
@@ -1,14 +1,33 @@
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include "game/game.h"
 #include "parser/parser.h"
 
 extern struct player* Player;
 
+void gameLoop();
+
 int main(int argc, char *argv[]) {
 	Player = InitGame();
-	char* message;
+
+	gameLoop();
 
 	DestroyGame(&Player);
 	return EXIT_SUCCESS;
+}
+
+void gameLoop(){
+	char  input[50];
+	char* message = NULL;
+	
+	while(true){
+		printf("\n> ");
+		fgets(input, 50, stdin);
+
+		message = Parse(input);
+		if(message == NULL) break;
+
+		puts(message);
+	}
+	puts("goodbye");
 }
