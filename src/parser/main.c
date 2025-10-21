d5d43d858f880d0103d4270d2a8b85ffc974946c
salim <salim.belkacemm@protonmail.com>
2025-10-21 23:41:40 +0100

    nothing burger

--- src/parser/main.c
+++ src/parser/main.c
@@ -3,19 +3,9 @@
 // for now
 #include "./includes.h"
 #include "./handlers.h"
-#include <stdlib.h>
-#include <string.h>
 
 extern struct player* Player;
 
-typedef struct grammar
-{
-	char* verbs[10];
-	char* objects[10];
-	char* (*Handle)(int);
-} grammar;
-#define GRAMMAR_SIZE 3
-enum{EXIT=0, GO, SEE,};
 
 grammar Grammar[] = {
 	{
--- src/parser/parser.h
+++ src/parser/parser.h
@@ -1,6 +1,15 @@
 #ifndef __PARSER__
 #define __PARSER__
 
+typedef struct grammar
+{
+	char* verbs[10];
+	char* objects[10];
+	char* (*Handle)(int);
+} grammar;
+
+#define GRAMMAR_SIZE 3
+enum{EXIT=0, GO, SEE,};
 char* Parse(char* raw);
 
 #endif
